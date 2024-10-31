5c1c2d40f004d21a6ed203df8dcc250992bdf574
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-10-31 09:07:37 +0700

    Lab1

--- Lab1/Hello.c
+++ Lab1/Hello.c
@@ -1,5 +1,12 @@
+//1. Khai Báo Thư Viện
 #include <stdio.h>
 //Hello World
+//2. Định nghĩa các hàm thủ tục.
+float giaiptbat2 (int a, int b, int c)
+{
+    
+}
+// Chạy vào hàm main
 int main() {
     printf("Hello World!\n");
     return 0;
