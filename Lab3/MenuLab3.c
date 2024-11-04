c1e2f1343e0b5d491cd39e4c59f75eb1d77c9c71
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-04 23:15:41 +0700

    Bug1

--- Lab3/MenuLab3.c
+++ Lab3/MenuLab3.c
@@ -2,11 +2,5 @@
 
 int main() {
     float so;
-    printf("Debug");
-    scanf("%d,so");
-    printf("Bebug2");
-    printf("Debug3");
-    printf("Debug3.1");
-    printf("Debug4");
-    printf("Debug5");
+    printf("Bug")
 }
