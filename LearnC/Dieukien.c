9a156eccb77b80d7bd9c2116e3b75441adf701e7
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-07 22:02:13 +0700

    Dieu Kien Long

--- LearnC/Dieukien.c
+++ LearnC/Dieukien.c
@@ -13,12 +13,20 @@ int main() {
     // Convert floating-point numbers to integers for modulo operation
     int a_int = (int)a;
     int b_int = (int)b;
+    int c_int = (int)c;
 
-    if ((a_int % 2) == 0 && (b_int % 2) == 0) {
-        printf("So a va b la so chan\n");
-    } else {
+    if ((a_int % 2 == 0) || (b_int % 2) == 0)
+    {
         printf("So a va b la so le\n");
+        if ((c_int % 2 == 0))
+        {
+            printf("So a b c la so le");
+        }
+        
+    }else {
+        printf("so a , b , c khong la so le");
     }
+    
 
     return 0;
 }
