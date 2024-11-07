9e18a708d43f725d8cd3c2d9ecc7a54bf1de8335
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-07 22:29:28 +0700

    Toi Uu If Else

--- LearnC/ThangNam.c
+++ LearnC/ThangNam.c
@@ -7,59 +7,18 @@ int main()
     printf("Thang?:");
     scanf("%d", &thang);
 
-    if (thang == 1)
+    if (thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 9 || thang == 11)
     {
-        printf("Thang 1 : 31 Ngay");
-    } 
-    else if (thang == 2)
-    {
-        printf("Thang 2 : 28 Ngay");
+        printf("31 Ngay");
     }
-    else if (thang == 3)
+    else if (thang == 4 || thang == 6 || thang == 8 || thang == 10 || thang == 12)
     {
-        printf("Thang 3 : 31 Ngay");
-    }
-    else if (thang == 4)
+        printf("30 Ngay");
+    } else if (thang == 2 )
     {
-        printf("Thang 4 : 30 Ngay");
-    }
-    else if (thang == 5)
-    {
-        printf("Thang 5 : 31 Ngay");
-    }
-    else if (thang == 6)
-    {
-        printf("Thang 6 : 30 Ngay");
-    }
-    else if (thang == 7)
-    {
-        printf("Thang 7 : 31 Ngay");
-    }
-    else if (thang == 8)
-    {
-        printf("Thang 8 : 30 Ngay");
-    }
-    else if (thang == 9)
-    {
-        printf("Thang 9 : 31 Ngay");
-    }
-    else if (thang == 10)
-    {
-        printf("Thang 10 : 30 Ngay");
-    }
-    else if (thang == 11)
-    {
-        printf("Thang 11 : 31 Ngay");
-    }
-    else if (thang == 12)
-    {
-        printf("Thang 12 : 30 Ngay");
-    }
-    else {
+        printf("28 ngay");
+    } else {
         printf("Thang khong hop le!");
     }
-    
-    
-    
     return 0;
 }
