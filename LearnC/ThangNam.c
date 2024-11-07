533c238d965ae9c6b32c0e8f75234db22d3d12bf
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-07 22:14:29 +0700

    ThangNam

---
+++ LearnC/ThangNam.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int thang;
+
+int main()
+{
+    printf("Thang?:");
+    scanf("%d", &thang);
+
+    if (thang == 1)
+    {
+        printf("Thang 1 : 31 Ngay");
+    } 
+    else if (thang == 2)
+    {
+        printf("Thang 2 : 28 Ngay");
+    }
+    else if (thang == 3)
+    {
+        printf("Thang 3 : 31 Ngay");
+    }
+    else if (thang == 4)
+    {
+        printf("Thang 4 : 30 Ngay");
+    }
+    else if (thang == 5)
+    {
+        printf("Thang 5 : 31 Ngay");
+    }
+    else if (thang == 6)
+    {
+        printf("Thang 6 : 30 Ngay");
+    }
+    else if (thang == 7)
+    {
+        printf("Thang 7 : 31 Ngay");
+    }
+    else if (thang == 8)
+    {
+        printf("Thang 8 : 30 Ngay");
+    }
+    else if (thang == 9)
+    {
+        printf("Thang 9 : 31 Ngay");
+    }
+    else if (thang == 10)
+    {
+        printf("Thang 10 : 30 Ngay");
+    }
+    else if (thang == 11)
+    {
+        printf("Thang 11 : 31 Ngay");
+    }
+    else if (thang == 12)
+    {
+        printf("Thang 12 : 30 Ngay");
+    }
+    else {
+        printf("Thang khong hop le!");
+    }
+    
+    
+    
+    return 0;
+}
