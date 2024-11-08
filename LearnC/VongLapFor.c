73cf26088bdf265514e773b0188e30c8475248f2
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-08 23:11:50 +0700

    LapFor

--- LearnC/VongLapFor.c
+++ LearnC/VongLapFor.c
@@ -6,6 +6,8 @@ int main()
     for (int i = 0; i < 10; i++)// Tạo biến i trong vòng for thì phạm vi nó nằm trong vòng lặp for thôi
     {
         printf("%d\n", i);
+        continue;
+        printf("Ndyu");
     }
     
     return 0;
