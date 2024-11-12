f30be586c31e2679c79324e3f9c6dc2648cf2329
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-12 14:04:04 +0700

    Dien Tich Hinh Tron

--- LearnC/NewbieDevTraining/BaiTap1/DienTichHinhTron.c
+++ LearnC/NewbieDevTraining/BaiTap1/DienTichHinhTron.c
@@ -4,14 +4,11 @@ int main() {
     const float PI = 3.14159;
     float R, S;
 
-    // Nhập bán kính từ bàn phím
     printf("Nhap ban kinh R cua hinh tron: ");
     scanf("%f", &R);
-
-    // Tính diện tích
+    // Dien tich hinh tron = PI * R binh.
     S = PI * R * R;
 
-    // In ra kết quả
     printf("Dien tich S cua hinh tron la: %.2f\n", S);
 
     return 0;
