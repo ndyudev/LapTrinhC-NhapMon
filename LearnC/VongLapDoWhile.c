1ff8a58b8531b99316655bea70e66255749c23dd
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-16 08:58:06 +0700

    VongLapWhileDo

---
+++ LearnC/VongLapDoWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    do
+    {
+        printf("Nhap lua chon cua ban:");
+        scanf("&d",a);
+    } while (a < 1);
+        printf("Lua chon cua ban la:%d\n");
+        return 0;
+    return 0;
+}
