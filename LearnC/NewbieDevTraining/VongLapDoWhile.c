dcffa0e4e8a87951ef54885c679bcf34c0e86add
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-18 16:56:03 +0700

    NBDT VongLap

---
+++ LearnC/NewbieDevTraining/VongLapDoWhile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,a,b,c;
+    do {
+        printf("Nhap vao lua chon cua ban");
+        scanf("%d",&i);
+    } while (i < 10);
+    
+    return 0;
+}
