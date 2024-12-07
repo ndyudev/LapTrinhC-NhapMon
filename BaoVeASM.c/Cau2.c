aa86b90804cf098cdc7e66ea8aa4a45b7f177562
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-12-07 09:57:36 +0700

    DebugASM

--- BaoVeASM.c/Cau2.c
+++ BaoVeASM.c/Cau2.c
@@ -7,16 +7,21 @@ int main() {
 
     int arr[n];
     float sum = 0;
+    int count = 0;
 
     for (int i = 0; i < n; i++) {
         printf("Nhap phan tu thu [%d]: ", i + 1);
         scanf("%d", &arr[i]);
-        sum += arr[i];
     }
 
-    float trungBinh = sum / n;
+    for (int i = 0; i < n; i += 2) {
+        sum += arr[i];
+        count++;
+    }
 
-    printf("Trung binh la: %.2f\n", trungBinh);
+    float trungBinh = sum / count;
+
+    printf("Trung binh cac phan tu o vi tri chan la: %.2f\n", trungBinh);
 
     return 0;
 }
