51f761e8e06d4e24f429cfce8ec30ad76f692366
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-12-08 23:33:39 +0700

    Cau 1 NBDT P2

--- NewbieDevTrain/Cau1.c
+++ NewbieDevTrain/Cau1.c
@@ -2,13 +2,13 @@
 
 int main() {
     int number;
-    printf("Enter a number: ");
+    printf("Nhap so muon in bang cuu chuong: ");
     scanf("%d", &number);
 
-    printf("Multiplication table of %d:\n", number);
+    printf("Bang cuu chuong tu 1 den 10 cua so %d la:\n", number);
     for (int i = 1; i <= 10; i++) {
-        printf("%d * %d = %d\n", number, i, number * i);
+        printf("%d. %d * %d = %d\n", i, number, i, number * i);
     }
-
+    
     return 0;
 }
