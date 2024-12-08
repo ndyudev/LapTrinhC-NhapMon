948a29d8cc49a9109a242adddf5da81049d433c8
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-12-08 22:58:08 +0700

    Cau 1 NBDT

---
+++ NewbieDevTrain/Cau1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("Multiplication table of %d:\n", number);
+    for (int i = 1; i <= 10; i++) {
+        printf("%d * %d = %d\n", number, i, number * i);
+    }
+
+    return 0;
+}
