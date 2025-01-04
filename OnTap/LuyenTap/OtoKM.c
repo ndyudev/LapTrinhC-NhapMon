4b50edd77ac2ac66de01b88bfe6601783d1ed95b
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-01-04 15:43:39 +0700

    Debug Oto KM

--- OnTap/LuyenTap/OtoKM.c
+++ OnTap/LuyenTap/OtoKM.c
@@ -8,28 +8,32 @@ float tinhQuangDuong(int hanhTrinh[], int soPhanTu) {
     for (int i = 0; i < soPhanTu; i++) {
         int giaTri = hanhTrinh[i];
 
-        if (giaTri > 0) { // Lên dốc
+        if (giaTri > 0) {
             float quangDuongDiDuoc = pin * 2.15;
             if (quangDuongDiDuoc >= giaTri){
                 quangDuong += giaTri;
-                pin -= giaTri/2.15;
+                pin -= (float)giaTri/2.15;
             }
             else{
                 quangDuong += quangDuongDiDuoc;
                 pin = 0;
                 break;
             }
-        } else if (giaTri == 0) { // Sạc pin
+        } else if (giaTri == 0) {
             pin += 25;
             if (pin > 100) {
                 pin = 100;
             }
-        } else { // Xuống dốc
+        } else {
             float pinDuocSac = fabs(giaTri) / 10.0;
             pin += pinDuocSac;
             if (pin > 100) {
                 pin = 100;
             }
+        }
+        if (pin <= 0){
+            pin = 0;
+            break;
         }
     }
 
@@ -37,7 +41,7 @@ float tinhQuangDuong(int hanhTrinh[], int soPhanTu) {
 }
 
 int main() {
-    int hanhTrinh[100]; // Khai báo mảng đủ lớn
+    int hanhTrinh[100];
     int soPhanTu = 0;
 
     printf("Nhap hanh trinh (nhap 0 de ket thuc): ");
@@ -45,7 +49,7 @@ int main() {
     while (scanf("%d", &giaTri) == 1 && giaTri != 0) {
         hanhTrinh[soPhanTu++] = giaTri;
     }
-    if (giaTri == 0 && soPhanTu == 0){
+        if (giaTri == 0 && soPhanTu == 0){
         printf("Hanh trinh trong!\n");
         return 1;
     }
